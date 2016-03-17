a4dcf295f04934257993949c8583eeff22aeb68c
Ilya <pseudo.ilya@gmail.com>
2016-03-17 20:56:05 +0300

    split into functions

--- deploy.sh
+++ deploy.sh
@@ -4,25 +4,52 @@ TABLE="./paths.table"
 declare -A paths succeed failed ignored
 declare -a temp 
 
-# read file & destination paths by line
-IFS=$'\n' read -d '' -r -a temp < "$TABLE"
+# TODO implement interactive mode
+#      like this:
+# where to link $filename? [default = $filepath]
+deploy()
+{
+    ;
+}
 
-for line in "${temp[@]}"; do
-    # delete in line comments
-    line=${line%%\#*}
-    # check whether the line not empty
-    if [[ -n "${line// }" ]]; then
-        # getting filename and destination path
-        # ****n' pipes, I couldn't make them work, thus:
-        echo $line > ".tmpfile"
-        read file dest < ".tmpfile"
-        # store values in a dict
-        paths["$file"]="$dest"
-    fi
-done; rm ".tmpfile"; unset temp
+check_ignored()
+{
+    ;
+}
 
-# iterate through the paths dict to ensure it's ok
-for file in "${!paths[@]}"; do
-    printf "%s %s\n" "$file" "${paths["$file"]}"
-done
+print_statistics()
+{
+    ;
+}
 
+read_config_table()
+{
+    # read file & destination paths by line
+    IFS=$'\n' read -d '' -r -a temp < "$TABLE"
+
+    for line in "${temp[@]}"; do
+        # delete in line comments
+        line=${line%%\#*}
+        # check whether the line not empty
+        if [[ -n "${line// }" ]]; then
+            # get filename and destination path
+            # ****n' pipes, I couldn't make them work, thus:
+            echo $line > ".tmpfile"
+            read file dest < ".tmpfile"
+            # store values in a dict
+            paths["$file"]="$dest"
+        fi
+    done; rm ".tmpfile"; unset temp
+
+    # iterate through the paths dict to ensure it's ok
+    for file in "${!paths[@]}"; do
+        printf "%s %s\n" "$file" "${paths["$file"]}"
+    done
+}
+
+main()
+{
+    ;
+}
+
+main
