9f3e3c9fc6ac378f625b9383beae358b587cf923
Ilya <pseudo.ilya@gmail.com>
2017-01-09 17:59:49 +0300

    Modify

--- .template.cc
+++ .template.cc
@@ -6,6 +6,7 @@
 using namespace std;
 
 #define MAX(T) (std::numeric_limits<T>::max())
+#define wi(x) (std::cerr << (#x) << " is " << (x) << std::endl)
 using i8  = int_fast8_t;
 using i16 = int_fast16_t;
 using i32 = int_fast32_t;
@@ -14,16 +15,11 @@ using u8  = uint_fast8_t;
 using u16 = uint_fast16_t;
 using u32 = uint_fast32_t;
 using u64 = uint_fast64_t;
-using st = size_t;
+using st  = size_t;
 const std::string nl = "\n";
 
-#define pb push_back
-#define eb emplace_back
-#define forn(n) fornn(_Temp_I_ ## __LINE__ ## __COUNTER__ ## _ ## __FILE__, (n))
-#define fornn(name, n) for (size_t name{}; name < n; ++name)
-#define what_is(x) (std::cerr << (#x) << " is " << (x) << std::endl)
-
-int main()
+int
+main(int argc, char** argv)
 {
     std::ios_base::sync_with_stdio(0);
     std::cin.tie(nullptr);  // disables automatic flush of std::cout before std::cin -- take care in interactive problems
@@ -31,7 +27,7 @@ int main()
     //std::ofstream  cout("TASKNAME.out");
     //std::cin.rdbuf(cin.rdbuf());
     //std::cout.rdbuf(cout.rdbuf());
-    /*************** GOOD LUCK ***************/
+    //std::cerr << "file i/o is used" << nl;
     
     // STARTHERE
 }
